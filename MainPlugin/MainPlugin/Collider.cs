2073d831bb7ae98d62cb18889122c76b97d5e427
DemonicSlitter <alessandro.falco.biella@gmail.com>
2018-02-21 23:06:14 +0100

    Reflection on boxes done

--- MainPlugin/MainPlugin/Collider.cs
+++ MainPlugin/MainPlugin/Collider.cs
@@ -1,12 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace MainPlugin
 {
-   public abstract class Collider
-   {
-       public STransform Transform;
+    public abstract class Collider
+    {
+        public STransform Transform;
 
-       public abstract bool IsColliding(Collider other);
+        public abstract bool IsColliding(Collider other);
+
+        public Vector2 GetReflection(Vector2 velocity, Collider other)
+        {
+            if (other.GetType() == typeof(BoxCollider))
+            {
+                BoxCollider box = (BoxCollider) other;
+                Vector2 middleDis = new Vector2(Math.Abs(Transform.Position.x - box.Transform.Position.x),
+                    Math.Abs(Transform.Position.y - box.Transform.Position.y));
+
+                if (Math.Abs(middleDis.x - box.Size.x / 2) <= Math.Abs(middleDis.y - box.Size.y / 2))
+                {
+                    return new Vector2(velocity.x * (-1), velocity.y);
+                }
+
+                return new Vector2(velocity.x, velocity.y * (-1));
+
+            }
+
+            if (other.GetType() == typeof(CircleCollider))
+            {
+
+            }
+            Server.Instance.Log("This is a Bug in GetReflection.. ur welcome :3");
+            return Vector2.down;
+        }
     }
 
 
@@ -28,10 +54,11 @@ namespace MainPlugin
             }
             if (other.GetType() == typeof(CircleCollider))
             {
-                return CollisionManager.CollideCircleAndBoxColliders(this, (CircleCollider)other);
+                return CollisionManager.CollideCircleAndBoxColliders(this, (CircleCollider) other);
             }
             return false;
         }
+
     }
 
     public class CircleCollider : Collider
