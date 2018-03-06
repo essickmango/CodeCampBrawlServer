8fa190fad07e1b7f624b3c0350b2cc76473910be
essickmango <timon@ylz.ch>
2018-03-06 15:44:42 +0100

    MapUpdate, BugFix, Character loses blood when getting hit

--- MainPlugin/MainPlugin/MapObject.cs
+++ MainPlugin/MainPlugin/MapObject.cs
@@ -4,18 +4,18 @@
     {
         public STransform Transform;
         public Collider Collider;
-        public ushort Id;
+        public ushort Type;
         public enum  Ids
         {
            Wall = 0 
         }
 
-        public static MapObject CreatePreset(Collider collider, ushort id)
+        public static MapObject CreatePreset(Collider collider, ushort type)
         {
             MapObject m = new MapObject();
             m.Transform = collider.Transform;
             m.Collider = collider;
-            m.Id = id;
+            m.Type = type;
 
             return m;
         }
